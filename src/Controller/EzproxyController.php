e2d61fdd7518b3b02b0634a1a81f5b3f75e14739
Ryan Eby <eby@ryaneby.com>
2018-01-04 16:57:08 -0500

    stub controller

---
+++ src/Controller/EzproxyController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\ezproxy\Controller\DefaultController.
+ */
+namespace Drupal\ezproxy\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class EzproxyController extends ControllerBase
+{
+
+}
