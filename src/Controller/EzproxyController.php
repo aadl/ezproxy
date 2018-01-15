13454755e1fcdd9939a9cad4d401c6c65cb08742
Ryan Eby <eby@ryaneby.com>
2018-01-15 18:49:01 -0500

    external authentication method

--- src/Controller/EzproxyController.php
+++ src/Controller/EzproxyController.php
@@ -6,8 +6,60 @@
 namespace Drupal\ezproxy\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\Entity\User;
+use Drupal\user\UserAuth;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class EzproxyController extends ControllerBase
 {
+    protected $userauth;
 
+    public function __construct(UserAuth $userauth)
+    {
+        $this->userauth = $userauth;
+    }
+
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('user.auth')
+        );
+    }
+    public function externalAuth(Request $request)
+    {
+        $response = new Response();
+        $method = $request->server->get('REQUEST_METHOD');
+
+        switch ($method) {
+            case 'GET':
+                $username = $request->query->get('ezuser');
+                $password = $request->query->get('ezpass');
+                break;
+            case 'POST':
+                $username = $request->request->get('ezuser');
+                $password = $request->request->get('ezpass');
+                break;
+        }
+        if (!$username || !$password) {
+            $response->setContent("+FAIL");
+            return $response;
+        }
+
+        $uid = $this->userauth->authenticate($username, $password);
+
+        if ($uid) {
+            $user = User::load($uid);
+            if ($user->hasPermission('access ezproxy content')) {
+                $roles = $user->getRoles(true);
+                $groups = implode('+', $roles);
+                $response_string = nl2br("+OK\nezproxy_group=".$groups);
+                $response->setContent($response_string);
+                return $response;
+            }
+        }
+        $response->setContent("+FAIL");
+        return $response;
+    }
 }
